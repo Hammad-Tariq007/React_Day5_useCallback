20967f36ad5fc3163da0687786d0f1d143e7191b
Hammad Tariq <hammadtariq0007@gmail.com>
2025-08-11 14:40:59 +0500

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { memo } from 'react'
+
+const Navbar = ({adjective}) => {
+  return (
+    console.log("Navbar rendered"),
+    <div>
+      I am a {adjective} Navbar
+    </div>
+  )
+}
+
+export default memo(Navbar)
